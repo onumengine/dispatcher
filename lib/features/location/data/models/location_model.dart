69d31f0820849d517ac32f32a3f5c45f36cfbb01
onumengine <onumdev@gmail.com>
2021-10-16 19:40:20 +0100

    define necessary methods in the location model

--- lib/features/location/data/models/location_model.dart
+++ lib/features/location/data/models/location_model.dart
@@ -1,1 +1,37 @@
-class LocationModel {}
+import 'dart:convert';
+
+import 'package:dispatcher/features/location/domain/entities/position.dart';
+
+class LocationModel extends Position {
+  double? latitude, longitude;
+
+  LocationModel.fromJsonString(String jsonString) {
+    Map decodedObject = jsonDecode(jsonString);
+    latitude = decodedObject['latitude'];
+    longitude = decodedObject['longitude'];
+  }
+
+  LocationModel.fromJsonMap(Map<dynamic, dynamic> locationInJson) {
+    latitude = locationInJson['latitude'];
+    longitude = locationInJson['longitude'];
+  }
+
+  String toJsonString() {
+    return jsonEncode({
+      'latitude': '$latitude',
+      'longitude': '$longitude',
+    });
+  }
+
+  Map<String, String> toJsonMap() {
+    return {
+      'latitude': '$latitude',
+      'longitude': '$longitude',
+    };
+  }
+
+  @override
+  String toString() {
+    return 'latitude: $latitude \nlongitude: $longitude';
+  }
+}
