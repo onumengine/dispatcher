84fcb71fc3a611d562310f77c5b3061a8f2038fa
onumengine <onumdev@gmail.com>
2021-10-20 17:50:22 +0100

    create a connection checker for NetworkInfoImpl

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -1,3 +1,14 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+
 abstract class NetworkInfo {
   Future<bool> get deviceIsConnected;
+}
+
+class NetworkInfoImplementation implements NetworkInfo {
+  final DataConnectionChecker connectionChecker;
+
+  NetworkInfoImplementation(this.connectionChecker);
+
+  @override
+  Future<bool> get deviceIsConnected => connectionChecker.hasConnection;
 }
