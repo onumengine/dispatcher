8b981a3cf36860e8e6e23dfde5cc26df90b28cd4
onumengine <onumdev@gmail.com>
2021-12-16 02:27:47 +0100

    delete router members

--- lib/core/navigation/route_names.dart
+++
@@ -1,6 +0,0 @@
-class RouteNames {
-  static const HOME = '/';
-  static const MAP = '/map';
-  static const SERVICE_SELECTION = '/select_service';
-  static const SET_LOCATIONS = '/set_locations';
-}
