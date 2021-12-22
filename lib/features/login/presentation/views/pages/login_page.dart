348c6258e77b172470045b7b7c84943a13b27489
onumengine <onumdev@gmail.com>
2021-12-22 16:01:43 +0100

    fix the responsiveness of the login page

--- lib/features/login/presentation/views/pages/login_page.dart
+++ lib/features/login/presentation/views/pages/login_page.dart
@@ -16,172 +16,178 @@ class LoginPage extends StatelessWidget {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Consumer<LoginViewModel>(
-        builder: (context, viewModel, child) => Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            LoginCanvas(),
-            Container(
-              height: screenSize.height / 2.25,
-              width: screenSize.width / 1.17,
-              padding: EdgeInsets.symmetric(horizontal: 26),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
-                    offset: Offset.zero,
-                    blurRadius: 4,
-                    spreadRadius: 4,
-                  ),
-                ],
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: screenSize.height / 47.2),
-                    Align(
-                      alignment: AlignmentDirectional.topStart,
-                      child: Text(
-                        'Login',
-                        style: GoogleFonts.poppins().copyWith(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                          height: 36 / 24,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: screenSize.height / 15.19),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.person_outline),
-                        hintText: 'Full Name',
-                      ),
-                    ),
-                    SizedBox(height: screenSize.height / 23.56),
-                    TextFormField(
-                      autocorrect: false,
-                      obscureText: !viewModel.passwordIsVisible,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock_outline),
-                        suffixIcon: GestureDetector(
-                          onTap: viewModel.togglePasswordVisibility,
-                          child: Icon(
-                            viewModel.passwordIsVisible
-                                ? Icons.visibility_rounded
-                                : Icons.visibility_off_outlined,
-                          ),
-                        ),
-                        hintText: 'Password',
-                      ),
-                    ),
-                    SizedBox(height: screenSize.height / 47.2),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Row(
-                          children: [
-                            Checkbox(
-                              value: viewModel.rememberMe,
-                              onChanged: (value) {
-                                viewModel.setRememberMe(value!);
-                              },
-                              fillColor: MaterialStateProperty.all<Color>(
-                                Theme.of(context).primaryColor,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                            ),
-                            Text(
-                              'Remember me',
-                              style: TextStyle(
-                                fontSize: 12,
-                                height: 18 / 12,
-                                color: Theme.of(context)
-                                    .inputDecorationTheme
-                                    .iconColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Text(
-                            'Forgot Password?',
-                            style: TextStyle(
-                              fontSize: 11,
-                              height: 16.5 / 11,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: screenSize.height / 11.49),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: screenSize.height / 37.3,
-              child: Container(
+        builder: (context, viewModel, child) =>
+            LayoutBuilder(builder: (context, constraints) {
+          return Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              LoginCanvas(),
+              Container(
+                height: screenSize.height / 2.25,
                 width: screenSize.width / 1.17,
-                child: Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(RouteNames.SIGN_UP);
-                      },
-                      child: Text('Login'),
-                      style: ButtonStyle(
-                        fixedSize: MaterialStateProperty.all<Size>(
-                          Size(MediaQuery.of(context).size.width, 50),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: screenSize.height / 40.72,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pushNamed(RouteNames.SIGN_UP);
-                      },
-                      child: RichText(
-                        text: TextSpan(
-                          text: 'Don\'t have an account? ',
-                          style: TextStyle(
-                            fontSize: 16,
-                            height: 24 / 16,
-                            fontFamily: GoogleFonts.poppins().fontFamily,
-                            color: ColorPalette.PALE_TEXT_COLOR,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'Sign Up',
-                              style: TextStyle(
-                                fontSize: 16,
-                                height: 24 / 16,
-                                fontFamily: GoogleFonts.poppins().fontFamily,
-                                color: ColorPalette.PRIMARY,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: screenSize.height / 37.3,
+                padding: EdgeInsets.symmetric(horizontal: 26),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      offset: Offset.zero,
+                      blurRadius: 4,
+                      spreadRadius: 4,
                     ),
                   ],
                 ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: screenSize.height / 47.2),
+                      Align(
+                        alignment: AlignmentDirectional.topStart,
+                        child: Text(
+                          'Login',
+                          style: GoogleFonts.poppins().copyWith(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            height: 36 / 24,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: screenSize.height / 15.19),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person_outline),
+                          hintText: 'Full Name',
+                        ),
+                      ),
+                      SizedBox(height: screenSize.height / 23.56),
+                      TextFormField(
+                        autocorrect: false,
+                        obscureText: !viewModel.passwordIsVisible,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock_outline),
+                          suffixIcon: GestureDetector(
+                            onTap: viewModel.togglePasswordVisibility,
+                            child: Icon(
+                              viewModel.passwordIsVisible
+                                  ? Icons.visibility_rounded
+                                  : Icons.visibility_off_outlined,
+                            ),
+                          ),
+                          hintText: 'Password',
+                        ),
+                      ),
+                      SizedBox(height: screenSize.height / 47.2),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: viewModel.rememberMe,
+                                onChanged: (value) {
+                                  viewModel.setRememberMe(value!);
+                                },
+                                fillColor: MaterialStateProperty.all<Color>(
+                                  Theme.of(context).primaryColor,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                              Text(
+                                'Remember me',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  height: 18 / 12,
+                                  color: Theme.of(context)
+                                      .inputDecorationTheme
+                                      .iconColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                fontSize: 11,
+                                height: 16.5 / 11,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Flexible(
+                        child: SizedBox(),
+                        fit: FlexFit.tight,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
+              Positioned(
+                bottom: screenSize.height / 37.3,
+                child: Container(
+                  width: screenSize.width / 1.17,
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed(RouteNames.SIGN_UP);
+                        },
+                        child: Text('Login'),
+                        style: ButtonStyle(
+                          fixedSize: MaterialStateProperty.all<Size>(
+                            Size(MediaQuery.of(context).size.width, 50),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: screenSize.height / 40.72,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pushNamed(RouteNames.SIGN_UP);
+                        },
+                        child: RichText(
+                          text: TextSpan(
+                            text: 'Don\'t have an account? ',
+                            style: TextStyle(
+                              fontSize: 16,
+                              height: 24 / 16,
+                              fontFamily: GoogleFonts.poppins().fontFamily,
+                              color: ColorPalette.PALE_TEXT_COLOR,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'Sign Up',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  height: 24 / 16,
+                                  fontFamily: GoogleFonts.poppins().fontFamily,
+                                  color: ColorPalette.PRIMARY,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: screenSize.height / 37.3,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          );
+        }),
       ),
     );
   }
