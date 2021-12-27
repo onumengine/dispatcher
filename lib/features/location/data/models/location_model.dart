e39178140fdf547c1f8d6a16832d8327a61b276c
onumengine <onumdev@gmail.com>
2021-12-27 04:04:45 +0100

    add all position's parameters to LocationEntity

--- lib/features/location/data/models/location_model.dart
+++ lib/features/location/data/models/location_model.dart
@@ -1,48 +1,85 @@
-import 'dart:convert';
-
 import 'package:dispatcher/features/location/domain/entities/location_entity.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LocationModel extends LocationEntity {
-  late final double latitude, longitude;
+  late final double? latitude,
+      longitude,
+      accuracy,
+      altitude,
+      heading,
+      speed,
+      speedAccuracy;
+  late final DateTime? timeStamp;
 
   LocationModel({
     required this.latitude,
     required this.longitude,
-  }) : super(latitude: latitude, longitude: longitude);
+    required this.accuracy,
+    required this.altitude,
+    required this.heading,
+    required this.speed,
+    required this.speedAccuracy,
+    required this.timeStamp,
+  }) : super(
+          latitude: latitude,
+          longitude: longitude,
+          accuracy: accuracy,
+          altitude: altitude,
+          heading: heading,
+          speed: speed,
+          speedAccuracy: speedAccuracy,
+          timeStamp: timeStamp,
+        );
 
   factory LocationModel.fromPosition(Position position) {
     return LocationModel(
       latitude: position.latitude,
       longitude: position.longitude,
+      accuracy: position.accuracy,
+      altitude: position.altitude,
+      heading: position.heading,
+      speed: position.speed,
+      speedAccuracy: position.speedAccuracy,
+      timeStamp: position.timestamp,
     );
   }
 
-  factory LocationModel.fromJsonString(String jsonString) {
-    return LocationModel(
-      latitude: jsonDecode(jsonString)['latitude'],
-      longitude: jsonDecode(jsonString)['longitude'],
+  Position toPosition() {
+    return Position(
+      longitude: longitude!,
+      latitude: latitude!,
+      timestamp: timeStamp,
+      accuracy: accuracy!,
+      altitude: altitude!,
+      heading: heading!,
+      speed: speed!,
+      speedAccuracy: speedAccuracy!,
     );
   }
 
-  factory LocationModel.fromJsonMap(Map<dynamic, dynamic> locationInJson) {
+  factory LocationModel.fromJson(Map<dynamic, dynamic> locationInJson) {
     return LocationModel(
       latitude: locationInJson['latitude'],
       longitude: locationInJson['longitude'],
+      accuracy: locationInJson['accuracy'],
+      altitude: locationInJson['altitude'],
+      heading: locationInJson['heading'],
+      speed: locationInJson['speed'],
+      speedAccuracy: locationInJson['speedAccuracy'],
+      timeStamp: locationInJson['timeStamp'],
     );
   }
 
-  String toJsonString() {
-    return jsonEncode({
-      'latitude': latitude,
-      'longitude': longitude,
-    });
-  }
-
-  Map<String, dynamic> toJsonMap() {
+  Map<String, dynamic> toJson() {
     return {
       'latitude': latitude,
       'longitude': longitude,
+      'accuracy': accuracy,
+      'altitude': altitude,
+      'heading': heading,
+      'speed': speed,
+      'speedAccuracy': speedAccuracy,
+      'timeStamp': timeStamp,
     };
   }
 
--- lib/features/location/domain/entities/location_entity.dart
+++ lib/features/location/domain/entities/location_entity.dart
@@ -1,16 +1,35 @@
 import 'package:equatable/equatable.dart';
 
 class LocationEntity extends Equatable {
-  final double latitude, longitude;
+  final double? latitude,
+      longitude,
+      accuracy,
+      altitude,
+      heading,
+      speed,
+      speedAccuracy;
+  late final DateTime? timeStamp;
 
   LocationEntity({
     required this.latitude,
     required this.longitude,
+    this.accuracy,
+    this.altitude,
+    this.heading,
+    this.speed,
+    this.speedAccuracy,
+    this.timeStamp,
   });
 
   @override
   List<Object?> get props => [
         latitude,
         longitude,
+        accuracy,
+        altitude,
+        heading,
+        speed,
+        speedAccuracy,
+        timeStamp,
       ];
 }
